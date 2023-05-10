fb238f122938ef7729e10a653ee61e3658823c90
a7u7a <esteban@esrs.co>
2023-05-10 20:04:51 +0100

    test

--- sunsetbase.py
+++ sunsetbase.py
@@ -261,31 +261,31 @@ class Sunset(SampleBase):
         #     sleep(1)
 
         # to test sun position
-        for t in range(24):
-            self.offset_canvas.Clear()
-            self.draw_sun(t)
-            self.offset_canvas = self.matrix.SwapOnVSync(self.offset_canvas)
-            sleep(1)
+        # for t in range(24):
+        #     self.offset_canvas.Clear()
+        #     self.draw_sun(t)
+        #     self.offset_canvas = self.matrix.SwapOnVSync(self.offset_canvas)
+        #     sleep(1)
 
-        # while True: 
-        #     if self.stock_data is not None:
-        #         futures = list(self.stock_data.keys())
-        #         self.offset_canvas.Clear()
-        #         upper = 40
-        #         lower = 60
-        #         # self.update_sky_gradient()
-        #         self.draw_sun(0)
-        #         for future in futures:
-        #             future_data = self.stock_data[future]
-        #             # c1 = (random.randint(0,255), random.randint(0,255), random.randint(0,255)) 
-        #             color = self.get_color_from_tickers(future)
-        #             self.plot_data(future_data, color, lower, upper)
-        #             # modify limits after plotting
-        #             h = int(panel_height/4)
-        #             upper += random.randint(h-4,h+4)
-        #             lower += random.randint(h-4,h+4)
-        #         self.offset_canvas = self.matrix.SwapOnVSync(self.offset_canvas)
-        #         sleep(5)
+        while True: 
+            if self.stock_data is not None:
+                futures = list(self.stock_data.keys())
+                self.offset_canvas.Clear()
+                upper = 40
+                lower = 60
+                # self.update_sky_gradient()
+                self.draw_sun(0)
+                for future in futures:
+                    future_data = self.stock_data[future]
+                    # c1 = (random.randint(0,255), random.randint(0,255), random.randint(0,255)) 
+                    color = self.get_color_from_tickers(future)
+                    self.plot_data(future_data, color, lower, upper)
+                    # modify limits after plotting
+                    h = int(panel_height/4)
+                    upper += random.randint(h-4,h+4)
+                    lower += random.randint(h-4,h+4)
+                self.offset_canvas = self.matrix.SwapOnVSync(self.offset_canvas)
+                sleep(5)
                 
 
 if __name__ == "__main__":
