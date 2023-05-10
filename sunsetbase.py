776bdc8618bd9577870bfa2d0e6821e8b7515d96
a7u7a <esteban@esrs.co>
2023-05-10 06:51:47 +0100

    test

--- sunsetbase.py
+++ sunsetbase.py
@@ -149,7 +149,7 @@ class Sunset(SampleBase):
         normalized_elevation = (sun_elevation + 1) / 2
 
         # convert to screen coordinates (0 at lowest, 64 at highest)
-        screen_position = int(normalized_elevation * 64)
+        screen_position = int(normalized_elevation * (panel_height*2))
 
         return screen_position
 
