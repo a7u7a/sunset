ab561758a5bc183ed4241b65f78dbb6c1299d5b0
a7u7a <esteban@esrs.co>
2023-05-12 20:52:01 +0100

    lala

--- sunsetbase.py
+++ sunsetbase.py
@@ -178,7 +178,7 @@ class Sunset(SampleBase):
         normalized_time = minutes_from_midnight / (24*60)
 
         # we use cos function to emulate the sun's movement, it returns 1 at midnight, 0 at 6AM, -1 at noon, 0 at 6PM and 1 at midnight again
-        sun_elevation = math.cos(2*math.pi * -normalized_time)
+        sun_elevation = math.cos(2*math.pi * normalized_time)
 
         # normalize sun elevation to a panel_height/2-panel_height scale (panel_height/2 at lowest, 0 at highest)
         screen_position = int((panel_height / 4) * (1-sun_elevation))
